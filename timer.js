97c8eac670346204265935ddf601243df4f5a92c
Ngan <ngannguyen141123@gmail.com>
2025-07-13 23:35:12 +1000

    add modal window for custom timer + save actions, the updated displayed time not complete yet

--- timer.js
+++ timer.js
@@ -1,17 +1,38 @@
 let displayedTimer = document.getElementById("timer-display");
+
+let pomodoro = 25;
+let shortBreak = 5;
+let longBreak = 10;
+
 // default values to start with, if set to null and zeros, gotta click on Pomodoro button so that the timer can be activated
-let timer = 60 * 25;
-let duration = 60 * 25;
-let remainingTime = 60 * 25;
+let timer = 60 * pomodoro;
+let duration = 60 * pomodoro;
+let remainingTime = 60 * pomodoro;
+
+let endSound = document.querySelector("#audio");
+
+// for keypress Enter
+let isRunning = false;
+
+window.addEventListener("keydown", function (event) {
+  if (event.code === "Enter") {
+    if (isRunning) {
+      pause();
+    } else {
+      start();
+    }
+    isRunning = !isRunning;
+  }
+});
 
 function setPomodoro() {
-  setTimer(60 * 25);
+  setTimer(60 * pomodoro);
 }
 function setShortBreak() {
-  setTimer(60 * 5);
+  setTimer(60 * shortBreak);
 }
 function setLongBreak() {
-  setTimer(60 * 10);
+  setTimer(60 * longBreak);
 }
 function setTimer(seconds) {
   clearInterval(timer); // clear the existing timer - stop
@@ -37,6 +58,7 @@ function start() {
       updateDisplay();
     } else {
       clearInterval(timer);
+      endSound.play();
       alert("Time's up!!");
     }
   }, 1000);
@@ -48,3 +70,35 @@ function reset() {
 function pause() {
   clearInterval(timer); // stop with the current displayed time, not clear things out completely
 }
+
+// modal
+const modal = document.getElementById("modal");
+const openModalBtn = document.getElementById("customTimer");
+const closeModalBtn = document.getElementById("close-modal");
+
+function openModal() {
+  modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";
+}
+const saveButton = document.getElementById("save-time");
+function saveCustomTime() {
+  event.preventDefault();
+  pomodoro = document.getElementById("customPomodoro").value;
+  shortBreak = document.getElementById("customShortBreak").value;
+  longBreak = document.getElementById("customLongBreak").value;
+  updateDisplay();
+  //   setPomodoro(pomodoro);
+  //   setShortBreak(shortBreak);
+  //   setLongBreak(longBreak);
+  closeModal();
+}
+saveButton.addEventListener("click", saveCustomTime);
+// add custom timer
+// add dark mode
+// add to-do list
+// add the sound
+// save the to-do for later
+// add full mode
+// Close modal if clicking outside the modal content (backdrop)
